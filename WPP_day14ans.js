a3c811cd5f1796d593cb2a9c5123a07e9219e79a
sibuspd <sibuspd@gmail.com>
2024-06-28 16:02:20 +0530

    WPP 14 updated

--- WPP_day14ans.js
+++ WPP_day14ans.js
@@ -1,11 +1,11 @@
 function cutOff(str){
     if(str.length%2==0)
-        return str.slice(0,str.length/2);
+        console.log(str.slice(0,str.length/2));
     else
         console.log("Give an even numbered word");
 }
 console.log(cutOff("MainTeri"));
-console.log(cutOff("MainTeriAak"));
+console.log(cutOff("MainTeriAakh"));
 console.log(cutOff("RoadieBanegaTu?"));
 
 
