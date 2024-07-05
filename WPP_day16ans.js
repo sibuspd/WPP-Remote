a8cc869aad156191042d18cecd31c37e09374aa1
sibuspd <sibuspd@gmail.com>
2024-07-05 15:55:16 +0530

    WPP 16 updated

--- WPP_day16ans.js
+++ WPP_day16ans.js
@@ -1,8 +1,10 @@
 function swap(arr){
+    if(arr.length>=0){
     let temp = arr[0];
     arr[0]=arr[arr.length-1];
     arr[arr.length-1] = temp;
     return arr;
+    } else return -1;
 }
 
 console.log(swap([1,2,3,4]));
